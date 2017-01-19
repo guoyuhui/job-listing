ecf5c610514b978966ca9d45bdffe516dcf8002c
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2017-01-19 14:30:59 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :jobs do
     resources :resumes
   end
 root 'jobs#index'
+
 namespace :admin do
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
 end
+
 end
