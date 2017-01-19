eee5dd6eaa59499d018b9dabe07a8b4eb1bdbbbe
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2017-01-19 13:52:41 +0800

    resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,9 +1,12 @@
 class Job < ApplicationRecord
   scope :published, -> { where(is_hidden: false)}
   scope :recent, -> { order('created_at DESC')}
-  
+
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0 }
+
+  has_many :resumes
+  
 end
