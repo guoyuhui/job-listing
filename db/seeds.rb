477f1ed34fcb5593eaf07275945eff0b4cef53ff
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2017-04-26 23:46:55 +0800

    add other_details_to_job&is_hidden

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.first
+User.create(email:"111@jb.com", password:"123456")
+u.is_admin = true
+u.save
