7e4fa9f75bbc82ae506d496974007b5bb6a92311
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2017-04-27 14:37:17 +0800

    resume controller&show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426151204) do
+ActiveRecord::Schema.define(version: 20170427055513) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20170426151204) do
     t.integer  "wage_upper_bound"
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
+  end
+
+  create_table "resumes", force: :cascade do |t|
+    t.integer  "job_id"
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
